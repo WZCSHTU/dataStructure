801ceb9230346e70c2236b45dd367b45704bf28f
WZCSHTU <3497679187@qq.com>
2022-12-30 15:48:30 +0800

    变更

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+/*** 中国象棋程序的入口
+ * @author WZCSHTU
+ * @version 1.0
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("            ");
     }
 }
